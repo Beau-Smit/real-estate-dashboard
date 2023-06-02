b575955c8d7aefbab6da29ae485dd8cdca966d73
Beau <beau.h.smit@gmail.com>
2023-06-01 20:15:43 -0500

    clean up code

--- Snowflake_ingest.py
+++ Snowflake_ingest.py
@@ -1,18 +1,14 @@
 import os
-import snowflake.connector
 import toml
 import pandas as pd
 import geopandas as gpd
 from snowflake.snowpark import Session
 
-table_name = "real_estate.locations.points_of_interest"
 ROOT = "C:/Users/Beau/Documents/GitHub/RealEstate"
-SHAPE_FILES = os.path.join(ROOT, "data", "raw", "shape_files")
-RAW = os.path.join(ROOT, "data", "raw")
 CLEANED = os.path.join(ROOT, "data", "processed")
 
 connection_parameters = toml.load(
-    os.path.join(ROOT, "streamlit", ".streamlit", "secrets.toml")
+    os.path.join(ROOT, "src", "streamlit", ".streamlit", "secrets.toml")
 )
 
 
@@ -22,17 +18,12 @@ def create_session():
 
 session = create_session()
 
+print("Reading location data.")
+
 # load location data
 df_location_combined = pd.read_pickle(
     os.path.join(CLEANED, "whats_nearby_location_data.pkl")
 )
-
-# load shape data
-df_shape_data_combined = pd.read_pickle(
-    os.path.join(CLEANED, "whats_nearby_shape_data.pkl")
-)
-df_shape_data_combined.GEOMETRY = df_shape_data_combined.GEOMETRY.astype(str)
-
 
 session.sql(
     """
@@ -41,10 +32,26 @@ session.sql(
         ,LABEL VARCHAR
         ,LATITUDE NUMBER(13, 10)
         ,LONGITUDE NUMBER(13, 10)
-        ,STATION_NAME VARCHAR
         )
     """
 ).collect()
+
+print("Writing location data to Snowflake.")
+
+session.write_pandas(
+    df=df_location_combined,
+    table_name="POINTS",
+    database="REAL_ESTATE",
+    schema="LOCATIONS",
+)
+
+print("Reading shape data.")
+
+# load shape data
+df_shape_data_combined = pd.read_pickle(
+    os.path.join(CLEANED, "whats_nearby_shape_data.pkl")
+)
+df_shape_data_combined.GEOMETRY = df_shape_data_combined.GEOMETRY.astype(str)
 
 session.sql(
     """
@@ -56,14 +63,7 @@ session.sql(
     """
 ).collect()
 
-
-# write data to Snowflake
-session.write_pandas(
-    df=df_location_combined,
-    table_name="POINTS",
-    database="REAL_ESTATE",
-    schema="LOCATIONS",
-)
+print("Writing shape data to Snowflake.")
 
 session.write_pandas(
     df=df_shape_data_combined,
